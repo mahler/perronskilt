659318b106e97a03d9ba23e461e9784043a6eea8
Flemming Mahler <flemming@mahler.io>
2012-12-18 16:33:15 +0100

    Closes #1 * Skiftet til at benytte oiorest til at hente s-tog stations liste. * Fjernet stations.txt da denne ikke længere benyttes.

--- opdaterUIC.php
+++ opdaterUIC.php
@@ -1,25 +1,13 @@
 <?php
 /**
- * Denne fil downloader stationer fra dsb med henblik på at finde stationernes UIC nummer.
- * Vi er kun interesseret i s-tog, hvorfor stationer vælges ud fra en whiteliste, der findes
- * i den lokale fil "stations.txt".
+ * Opdaterer cachet liste af s-togs stationer på baggrund af data fra geo.oiorest.dk
  */
 $targetFilename = 'stations.csv';
-$fileContent    = file_get_contents('stations.txt');
-$dataLines      = explode("\n", $fileContent);
-$stogStation    = array();
 
 setlocale(LC_ALL, 'da_DK.UTF8');
 
-foreach ($dataLines as $stationRow) {
-  /* Check at det ikke er en kommentar */
-  if (!(substr($stationRow, 0, 1) == '#') && !empty($stationRow)) {
-      $stogStation[$stationRow] = 1;
-  }
-}
-
-/* Dette kald returnerer alle stationer (incl. et par tyske og svenske). */
-$oDataUrl = 'http://traindata.dsb.dk/stationdeparture/opendataprotocol.svc/Station()?$format=json';
+/* Dette kald returnerer alle s-tog stationer  */
+$oDataUrl = 'http://geo.oiorest.dk/holdepladser.json?holdepladstype=1';
 
 $cUrl = curl_init();
 curl_setopt($cUrl, CURLOPT_URL, $oDataUrl);
@@ -29,17 +17,14 @@ curl_setopt($cUrl, CURLOPT_RETURNTRANSFER, 1);
 $curlData = curl_exec($cUrl);
 curl_close($cUrl);
 
-$rawAnswer   = json_decode($curlData);
-$rawStations = $rawAnswer->d;
-
+$rawAnswer  = json_decode($curlData);
 $stogBuffer = array();
-foreach ($rawStations as $stationRow) {
-  $stationsNavn = $stationRow->Name;
+foreach ($rawAnswer as $stationRow) {
+	// Data fra oiorest postfix'er alle stationer med " st". Det fjerner vi lige.
+	$sNavn = $stationRow->navn;
+	$sNavn = preg_replace('/ (st)$/', '', $sNavn);
 
-  /* Er det en s-tog station? */
-  if (isset($stogStation[$stationsNavn])) {
-      $stogBuffer[$stationsNavn] = array( 'navn' => $stationsNavn, 'uic' => $stationRow->UIC);
-  }
+	array_push($stogBuffer, array( 'navn' => $sNavn, 'uic' => $stationRow->holdepladsnr));
 }
 
 /* Sorter liste alfabetisk */
@@ -48,6 +33,6 @@ ksort($stogBuffer);
 /* Data i filen skrives i UTF8 format (json data kommer ind som UTF8). */
 $fp = fopen($targetFilename, 'w');
 foreach ($stogBuffer as $stogStation) {
-  fputcsv($fp, $stogStation);
+	fputcsv($fp, $stogStation);
 }
 fclose($fp);
